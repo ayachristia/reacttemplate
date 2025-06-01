a30be558ed03102b903f560223eefaa7ea976525
iamayac <ayachristia@gmail.com>
2025-06-01 23:57:11 +0200

    move handlesubmit to action utility and added on in router

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,58 +1,14 @@
-import { useState } from "react";
-import { Form, useNavigate } from "react-router";
-import { z } from "zod/v4";
-
-const contactSchema = z.object({
-    name: z.string().min(2, "Name is required"),
-    email: z.email(),
-    message: z.string().min(1, "Message is required")
-})
-
-// const input = {  }
+import { Form, useActionData } from "react-router";
 
 export default function Contact() {
-    const [errors, setErrors] = useState({});
-    const navigate = useNavigate()
+    const errors = useActionData();
 
-    const handleSubmit = async (event)=>{
-        event.preventDefault();
-        
-        const formData = new FormData(event.target)
-        const data = Object.fromEntries(formData.entries())
-        console.log("Form submitted:", data);
-
-        //fetch... method post
-        const result = contactSchema.safeParse(data);
-        if(!result.success){
-            const errors = z.treeifyError(result.error)
-            console.log(errors)
-            setErrors(errors.properties)
-        } else{
-            setErrors({})
-
-        const response = await fetch("https://jsonplaceholder.typicode.com/users", {
-            method:"POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(result.data)
-        })
-        if(!response.ok){
-                throw new Error("Could not save data")
-        } 
-            navigate("/tak")
-            
-        } 
-        
-    }
-
-    
     return (
         <>
         
         <h1>Contact</h1>
 
-        <Form method="post" onSubmit={handleSubmit}>
+        <Form method="post" >
             <div className="form-group">
                 <label htmlFor="name">Name:</label>
                 <p>{errors && errors?.name?.errors[0]}</p>
